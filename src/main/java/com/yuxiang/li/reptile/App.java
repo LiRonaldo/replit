add86edfd8ecc8f8586445173d2f201c74cb290c
liyuxiang <yuxiang.li@yimifudao.com>
2019-01-05 04:43:32 +0800

    xiugai 112321

--- src/main/java/com/yuxiang/li/reptile/App.java
+++ src/main/java/com/yuxiang/li/reptile/App.java
@@ -15,7 +15,7 @@ public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        System.out.println("dev-10-10");
+        System.out.println("dev-10-10-10");
     }
 }
 
